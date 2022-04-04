2fadc352c3ccbd3d309cca8aa7c8dd5b1d33aba4
cheatsheet1999 <37787994+cheatsheet1999@users.noreply.github.com>
2022-04-04 13:54:36 -0700

    Add files via upload

---
+++ Yuelin-React-Unit-2/homework2-list/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './Components/App.css';
+// import './Components/Header.css';
+import UseEffectAPI from './Components/UserCard';
+
+
+
+function App() {
+  return (
+    <div>
+      <UseEffectAPI/>
+    </div>
+  );
+}
+
+export default App;
