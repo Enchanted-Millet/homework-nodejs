a5f6788b301b172dad31337a55fd057c9ac0d26c
AaronZ <rocheers@hotmail.com>
2022-04-13 20:10:34 -0700

    U6 hw1

--- reactjs/src/U6/hw1/index.js
+++ reactjs/src/U6/hw1/index.js
@@ -4,18 +4,11 @@ import List from './List';
 import { addItem, toggleItem, toggleItems } from './redux/actions/todos';
 import './style.css';
 
-// let ID = 1;
-
-const findOneById = (items, id) => {
-    return items.find(item => item.id === id);
-};
-
 const TodoList = () => {
     const [input, setInput] = useState('');
 
     //! use `useSelector` hook to get required state in redux store
     const items = useSelector(state => state);
-    // const [items, setItems] = useState([]);
 
     const dispatch = useDispatch();
 
@@ -26,43 +19,26 @@ const TodoList = () => {
     const handleKeyDown = e => {
         if (e.keyCode === 13) {
             const newItem = {
-                // id: ID++,
                 name: input,
                 completed: false
             };
 
             //! call action creator to update redux store
             dispatch(addItem(newItem));
-            // setItems([...items, newItem]);
-
             setInput('');
         }
     };
 
     const handleCheckItem = id => {
-        // const copyItems = [...items];
-        // const item = findOneById(copyItems, id);
-        // if (item) {
-        //     item.completed = !item.completed;
-        // }
-
         dispatch(toggleItem(id));
-        // setItems(items => [
-        //     ...items.slice(0, id - 1),
-        //     { ...items[id - 1], completed: !items[id - 1].completed },
-        //     ...items.slice(id)
-        // ]);
-        // setItems(copyItems);
     };
 
     const handleClearAllCompleted = () => {
         dispatch(toggleItems(false));
-        // setItems(items.map(item => ({ ...item, completed: false })));
     };
 
     const handleMarkAllDone = () => {
         dispatch(toggleItems(true));
-        // setItems(items.map(item => ({ ...item, completed: true })));
     };
 
     let remain = items.filter(item => !item.completed).length;
