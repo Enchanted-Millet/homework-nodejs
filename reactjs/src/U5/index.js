8323a943c2ac8e084303274977880cc6afc71689
AaronZ <rocheers@hotmail.com>
2022-06-09 12:54:45 -0700

    upload U5 hw4 traffic light simulator

--- reactjs/src/U5/index.js
+++ reactjs/src/U5/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import StaticNews from './hw1'
 import GitHubPage from './hw2'
 import ColorComponent from './hw3'
+import TrafficLightSimulator from './hw4'
 
 export default function U5() {
     return (
         // <StaticNews />
         // <GitHubPage />
-        <ColorComponent />
+        // <ColorComponent />
+        <TrafficLightSimulator />
     )
 }
