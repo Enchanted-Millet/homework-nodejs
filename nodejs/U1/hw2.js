0e8e4da49c3df7bd4039046047dac398c8b09d90
AaronZ <rocheers@hotmail.com>
2022-08-07 22:42:15 -0700

    refactor name

--- nodejs/U1/hw2.js
+++ nodejs/U1/hw2.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-// node hw2.js ./ txt
+/**
+ * How to run this file:
+ * node hw2.js <path_to_folder> <file extension>
+ * e.g. node hw2.js ./ txt
+ */
 const customArgs = process.argv.slice(2);
 const [dirPath, extension] = customArgs;
 
