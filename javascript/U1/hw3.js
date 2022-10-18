8706baad5614e11de798ce8a06c48d16998870fb
Enchanted-Millet <rocheers@hotmail.com>
2022-10-17 21:39:53 -0700

    fix typo

--- javascript/U1/hw3.js
+++ javascript/U1/hw3.js
@@ -9,7 +9,7 @@ console.log(1 + +'2' + '2'); // '2' -> 2 -> 1 + 2 -> 3 + '2' -> '32'
 console.log(1 + -'1' + '2'); // '02'
 console.log(+'1' + '1' + '2'); // '11' -> '112'
 
-console.log('A' - 'B' + '2'); // Not a Number
+console.log('A' - 'B' + '2'); // NaN + '2' -> 'NaN2'
 
 console.log('A' - 'B' + 2); // NaN + 2
 
