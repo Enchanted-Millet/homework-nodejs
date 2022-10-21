0a2327c6262e5e831edf38e2a113c9cdcbf85ab5
djjia <59391439+djjia@users.noreply.github.com>
2022-10-21 14:59:17 -0400

    finish U4/ hw2-5

--- javascript/U4/hw4.js
+++ javascript/U4/hw4.js
@@ -1,65 +1,23 @@
-// class MyClass {
-
-// }
-
-// const Singleton = (function () {
-//     let instance;
-
-//     function createInstance() {
-//         let classInstance = new MyClass();
-//         return classInstance;
-//     }
-
-//     return {
-//         getInstance: function () {
-//             if (!instance) {
-//                 instance = createInstance;
-//             }
-//             return instance;
-//         }
-//     }
-// })();
-
-// module.export = Singleton;
-
-// ES5
-// var Singleton = (function () {
-//     var instance = null;
-
-//     function Singleton() {
-//         if (!instance) {
-//             instance = this;
-//             this.obj = {
-//                 version: Math.floor(Math.random() * 2000)
-//             }
-//         }
-//         return instance;
-//     }
-
-//     return Singleton;
-// })();
-
-// Singleton.prototype.get = function (key) {
-//     return this.obj[key];
-// }
-
-// ES6 / 2015
-class Singleton {
-    constructor() {
-        if (Singleton.instance instanceof Singleton) {
-            return Singleton.instance;
-        }
-        this.obj = {
-            version: Math.floor(Math.random() * 2000)
-        };
-        Singleton.instance = this;
+//ES5
+function singleton(name) {
+    if (!singleton.instance) {
+        this.name = name
+        singleton.instance = this
     }
-    get(key) {
-        return this.obj[key];
+    return singleton.instance
+}
+
+//ES6
+class singleton {
+    constructor(name) {
+        if (!singleton.instance) {
+            this.name = name
+            singleton.instance = this
+        }
+        return singleton.instance
     }
 }
 
-const a = new Singleton();
-const b = new Singleton();
-console.log(a.get('version'));
-console.log(b.get('version'));
+const a = new singleton('a')
+const b = new singleton('b')
+console.log(a === b)
--- javascript/U4/hw5.js
+++ javascript/U4/hw5.js
@@ -1,47 +1,42 @@
 class TwoSum {
     constructor() {
-        this.numbers = [];
+        this.numArr = []
     }
-    add(n) {
-        this.numbers.push(n);
+    add(num) {
+        return this.numArr.push(num)
     }
-
-    find(n) {
-        const obj = {};
-        for (let num of this.numbers) {
-            if (num in obj) {
-                return true;
-            } else {
-                obj[n - num] = num;
+    find(num) {
+        for (let i = 0; i < this.numArr.length; i++) {
+            for (let j = 0; j < this.numArr.length; j++) {
+                if (num - this.numArr[i] === this.numArr[j] && i !== j) {
+                    return true
+                }
+            }
+            
+        }
+        return false
+    }
+    remove(num) {
+        let count = 0
+        for (let i = this.numArr.length; i > 0; i--) {
+            if (this.numArr[i] === num) {
+                this.numArr.splice(i, 1);
+                count += 1
             }
         }
-        return false;
+        return count
     }
+    
+} 
 
-    remove(n) {
-        let count = 0;
-        // this.numbers = this.numbers.filter(num => {
-        //     if (num === n) count++;
-        //     return num !== n;
-        // });
-        for (let idx in this.numbers) {
-            if (this.numbers[idx] === n) {
-                count++;
-                this.numbers.splice(idx, 1);
-                // slice, splice
-            }
-        }
-        return count;
-    }
-}
+const twosum = new TwoSum()
+twosum.add(1)
+twosum.add(3)
+twosum.add(3)
+twosum.add(5)
+twosum.add(3)
+console.log(twosum.find(4))
+console.log(twosum.find(7))
 
-const sol = new TwoSum();
-sol.add(1);
-sol.add(2);
-sol.add(3);
-sol.add(4);
-sol.add(4);
-sol.add(5);
-console.log(sol.find(6));
-console.log(sol.remove(4));
-console.log(sol.find(8));
+console.log(twosum.remove(3))
+console.log(twosum.find(4))
